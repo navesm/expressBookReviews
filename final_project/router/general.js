9e935b4152f48d8d3c0869144591c39493133aa2
navesm <stevennave@gmail.com>
2023-05-31 11:38:18 -0400

    project progress on endpoint filters

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,31 +12,48 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4));
+  return res.status(300).json({message: "Here is the list of available books!"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  res.send(books[isbn]);
+  return res.status(200).json({message: "Here are the books by ISBN"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author
+  let booksKeys = Object.keys(books);
+  let filteredBooks = booksKeys.map(key =>
+    books[key]).filter(book => book.author === author);
+  res.send(filteredBooks);
+  return res.status(200).json({message: "Here are the books by that author"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+  let booksKeys = Object.keys(books);
+  let filteredBooksByTitle = booksKeys.map(key =>
+    books[key]).filter(book => book.title === title);
+  res.send(filteredBooksByTitle);
+  return res.status(200).json({message:"Here is the title you requested"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
+  let isbn = req.params.isbn;
+  let reviews = req.params.reviews;
+  let booksKeys = Object.keys(books);
+  let filteredBooksByIsbn = booksKeys.map(key =>
+    books[key]).filter(book => book.isbn === isbn);
+  res.send(filteredBooksByIsbn[reviews])
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
